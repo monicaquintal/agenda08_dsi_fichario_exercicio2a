593912f31af9e6cd843978bc34f7cbca493d2196
Mônica Zungalo Quintal <111545468+monicaquintal@users.noreply.github.com>
2022-09-25 21:57:24 -0300

    Add files via upload

---
+++ Atividade2_fichario_dsi_agenda08.java
@@ -0,0 +1,57 @@
+package atividade2_fichario_dsi_agenda08;
+
+public class Atividade2_fichario_dsi_agenda08 {
+    
+    public static void main(String[] args) {
+        // declaração de variaveis
+        int multiplo = 0;
+        int resultado;
+    
+    System.out.println("Tabuada da Giovanna:"); 
+    
+    System.out.println("Tabuada do 6:");
+    do {
+    resultado = 6 * multiplo;
+    System.out.println("6 x " + multiplo +  " = " + resultado);
+    multiplo++;
+} while (multiplo <= 10);
+ 
+    multiplo = 0;
+    System.out.println("Tabuada do 7:");   
+    
+    do {
+    resultado = 7 * multiplo;
+    System.out.println("7 x " + multiplo +  " = " + resultado);
+    multiplo++;
+} while (multiplo <= 10);
+    
+    multiplo = 0;
+    System.out.println("Tabuada do 8:");
+    
+    do {
+    resultado = 8 * multiplo;
+    System.out.println("8 x " + multiplo +  " = " + resultado);
+    multiplo++;
+} while (multiplo <= 10);
+
+    multiplo = 0;
+    System.out.println("Tabuada do 9:");
+    
+    do {
+    resultado = 9 * multiplo;
+    System.out.println("9 x " + multiplo +  " = " + resultado);
+    multiplo++;
+} while (multiplo <= 10);
+    
+    
+    multiplo = 0;
+    System.out.println("Tabuada do 10:");
+    
+    do {
+    resultado = 10 * multiplo;
+    System.out.println("10 x " + multiplo +  " = " + resultado);
+    multiplo++;
+} while (multiplo <= 10);
+    
+    }
+        }
